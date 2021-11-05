426f454ee6e2a6888374503ace7bf94a4cee0b80
eliw13 <82398671+eliw13@users.noreply.github.com>
2021-11-04 19:12:36 -0500

    Add files via upload

--- Engine/Engine.java
+++ Engine/Engine.java
@@ -24,32 +24,33 @@ public class Engine {
         Random rand = new Random(); // Random number generator
 
         for (int i = 0; i < 100; i++) {
-            int n = rand.nextInt(100); // 0 <= n <= 99
-            String b = convert(n); // binary
+            int n = rand.nextInt(100); // generates numbers (n) ranging anywhere 0 <= n <= 99
+            String b = convert(n); // converts the number to a binary string
             out.println(b); // write to file
         }
-        out.close(); // close starter file
+        out.close(); // close main file
 
         try {
-            File evenFile = new File("evenFile.txt");
-            File tenFile = new File("tenFile.txt");
-            File threeFile = new File("threeFile.txt");
-            FileReader fr = new FileReader(file);
-            PrintWriter pw2 = new PrintWriter(new FileWriter(evenFile));
+            File evenFile = new File("evenFile.txt"); // Creates the file containing even binary numbers
+            File tenFile = new File("tenFile.txt"); // Creates the file containing binary numbers with '10'
+            File threeFile = new File("threeFile.txt"); // Creates the file containing binary numbers divisible by 3
+            FileReader fr = new FileReader(file); // Creates a file reader to read the files
+            PrintWriter pw2 = new PrintWriter(new FileWriter(evenFile)); // Creates a file writer to write to the even
+                                                                         // file
             PrintWriter pw3 = new PrintWriter(new FileWriter(tenFile));
             PrintWriter pw4 = new PrintWriter(new FileWriter(threeFile));
-            String str = "";
+            String str = ""; // String to store the file contents
             int x;
 
-            while ((x = fr.read()) != -1) {
-                str += (char) x;
+            while ((x = fr.read()) != -1) { // Reads the file
+                str += (char) x; // Adds the file contents to the string
             }
 
             // Patterns
-            String pattern1 = "(0|1)*0"; // pattern for even numbers
-            String pattern2 = "(0*1*100*1*)"; // pattern for
+            String pattern1 = "(0|1)*0"; // Regular Expression pattern for even numbers
+            String pattern2 = "(0*1*100*1*)"; // RE pattern for
                                               // numbers containing '10'
-            String pattern3 = "(1(01*0)*1|0)*"; // pattern for numbers
+            String pattern3 = "(1(01*0)*1|0)*"; // RE pattern for numbers
                                                 // divisible by 3
 
             // Compiling Patterns
@@ -57,7 +58,7 @@ public class Engine {
             Pattern p2 = Pattern.compile(pattern2); // compile pattern2
             Pattern p3 = Pattern.compile(pattern3); // compile pattern3
 
-            // Matchers
+            // Three Separate Matchers for Above Patterns
             Matcher m = p.matcher(str); // match pattern
             Matcher m2 = p2.matcher(str); // match pattern2
             Matcher m3 = p3.matcher(str); // match pattern3
@@ -72,12 +73,14 @@ public class Engine {
             while (m3.find()) {
                 pw4.write(m3.group() + "\n"); // write to file
             }
+
             // Closing all of the file readers and writers
-            fr.close(); // close the file
-            pw2.close(); // close file
-            pw3.close(); // close file
-            pw4.close(); // close file
+            fr.close();
+            pw2.close();
+            pw3.close();
+            pw4.close();
             System.out.println("File reading and writing both done.");
+
         } catch (IOException e) {
             System.out.println("Error"); // handle exception
         }
